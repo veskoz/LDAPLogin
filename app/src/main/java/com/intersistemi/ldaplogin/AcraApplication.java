06f0f08c19b6a4dc2959b84cc0d8d37450746495
Andrea Vescovi <vescosauro@hotmail.it>
2020-07-27 20:25:54 +0200

    code cleanup

--- app/src/main/java/com/intersistemi/ldaplogin/AcraApplication.java
+++ app/src/main/java/com/intersistemi/ldaplogin/AcraApplication.java
@@ -14,11 +14,11 @@ import org.acra.sender.HttpSender;
 @AcraCore(reportFormat = StringFormat.JSON)
 
 @AcraHttpSender(uri = "http://192.168.1.16:8080/report",
-                basicAuthLogin = "czhQb8l3bVfSabd8",
-                basicAuthPassword = "CZiw77DU8tLH3RVm",
-                httpMethod = HttpSender.Method.POST)
+        basicAuthLogin = "czhQb8l3bVfSabd8",
+        basicAuthPassword = "CZiw77DU8tLH3RVm",
+        httpMethod = HttpSender.Method.POST)
 
-@AcraToast(resText=R.string.acra_toast_text)
+@AcraToast(resText = R.string.acra_toast_text)
 
 public class AcraApplication extends Application {
     @Override
